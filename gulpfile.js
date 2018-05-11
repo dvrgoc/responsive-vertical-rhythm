80652cab374ccb16c2d9988bc504bf3361bfc657
Danijel Vrgoc <danijel.vrgoc@inchoo.net>
2018-05-11 13:01:30 +0200

    v rhythm on mobile

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp')
+    , sass = require("gulp-sass")
+    , sourcemaps = require('gulp-sourcemaps')
+    , autoprefixer = require('gulp-autoprefixer')
+    , notify = require('gulp-notify')
+    , plumber = require('gulp-plumber')
+    , browserSync = require('browser-sync')
+    , autoprefixer_support = ['last 2 version']
+;
+
+gulp.task('styles', function() {
+    return gulp.src('scss/*.scss')
+        .pipe(plumber())
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'expanded'
+        }))
+        .pipe(autoprefixer(autoprefixer_support))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.stream())
+        .pipe(notify({
+            message: '"styles" task complete'
+        }));
+});
+
+//watch
+gulp.task('watch', function () {
+    // Watch .scss files
+    gulp.watch('scss/**/*.scss', ['styles']);
+});
+
+gulp.task('browserSync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task('default', ['styles', 'watch', 'browserSync']);
---
+++ js/script.js
@@ -0,0 +1,17 @@
+WebFontConfig = {
+    google: {
+        families: [
+            'Fira+Sans:400,700:latin',
+            'Lobster:400,700:latin',
+            'Roboto+Slab:400,700:latin'
+        ]
+    }
+};
+(function () {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
